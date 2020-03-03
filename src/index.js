be46982b2ab6b420405b6cf15abef6ee990c8972
spytni1212 <avdeenkoartem1212@gmail.com>
2020-03-03 18:50:46 +0300

    compate task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+  if (matrix === undefined) {
+      return [];
+  } else {
+      for (let i = 0; i < matrix.length; i++) {
+          if (i === 1 || i === 3 ) {
+              matrix[i].reverse();
+          }
+          for (let j = 0; j < matrix[i].length; j++) {
+              arr.push(matrix[i][j]);
+          }
+      }
+  }
+  return arr;
 }
